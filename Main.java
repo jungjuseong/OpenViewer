b12339e5d8942f19f23de0c90721fa0ad088c5fe
jung juseong <jungjuseong@gmail.com>
2016-01-25 15:26:46 +0900

    명령행 인자가 있으면 메시지를 출력한다.

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 package com.mypackage;
+
 public class Main {
 
 	public static void main(String args) {
 
-		System.out.println("Hello my package main");
+		if (args.length() > 1)
+			System.out.println("Hello " + args[1]);
+		else
+			System.out.println("Hello my package main");
 	}
 }
