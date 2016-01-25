4f94b12afe0c58a8675d5522d5c4de05b21c5d5a
정주성 <jungjuseong@gmail.com>
2016-01-25 17:36:10 +0900

    Add new file - mmm.java

---
+++ mmm.java
@@ -0,0 +1,13 @@
+// hello
+package com.mypackage;
+
+public class Main {
+
+	public static void main(String args) {
+
+		if (args.length() > 1)
+			System.out.println("Hello " + args[1]);
+		else
+			System.out.println("Hello my package main");
+	}
+}
